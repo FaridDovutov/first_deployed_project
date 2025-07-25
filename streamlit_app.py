2816e763fca6150135a11616e9326923db4306b4
FaridDovutov <barcapamir.dovutov@gmail.com>
2025-07-25 17:05:55 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ y = df['species']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 encoder = ce.TargetEncoder(cols=['island', 'sex'])
-X_train_encoder = encoder.fit_transform(X_train, y_train)
+X_train_encoded = encoder.fit_transform(X_train, y_train)
 X_test_encoded = encoder.transform(X_test)
 
 models = {
