a14cd3df6433dda12cbaa8f6bab2d2ac70501b69
FaridDovutov <barcapamir.dovutov@gmail.com>
2025-07-25 17:04:15 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ with col2:
   st.plotly_chart(fig2, use_container_width=True)
 X = df.drop(['species'], axis=1)
 y = df['species']  
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, randome_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 encoder = ce.TargetEncoder(cols=['island', 'sex'])
 X_train_encoder = encoder.fit_transform(X_train, y_train)
