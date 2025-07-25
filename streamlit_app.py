0d127ebf32faf5379d56b1969e22e7f20abe9994
FaridDovutov <barcapamir.dovutov@gmail.com>
2025-07-25 16:03:03 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-iport pandas as pd
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.tree import DecisionTreeClassifier
